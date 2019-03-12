27d3b11901da5dc2c019ca83b7d112dfca047ea8
Alexander Kozlov <akozlov@kth.se>
2019-03-12 12:51:40 +0100

    20190312

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
-from setuptools import setup
+import setuptools
 
-setup(name='morphon',
-      version='0.0.2',
-      description='neuron morphology processing tools',
-      author='Alexander Kozlov',
-      author_email='akozlov@kth.se',
-      license='GPLv3',
-      packages=['morphon'],
-      scripts=[
-          'apps/mcheck',
-          'apps/mfind',
-          'apps/mmeter',
-          'apps/mmod',
-          'apps/mrep',
-          'apps/msholl',
-          'apps/mshow',
-      ],
-      install_requires=[
-          'numpy',
-          'matplotlib',
-      ],
-      zip_safe=False)
+setuptools.setup(name="morphon",
+    version="0.0.2",
+    description="Neuron morphology processing tools",
+    long_description="Neuron morphology processing tools",
+    author="Alexander Kozlov",
+    author_email="akozlov@kth.se",
+    url="https://github.com/a1eko/morphon",
+    license="GPLv3",
+    packages=setuptools.find_packages(),
+    scripts=[
+        "apps/mcheck",
+        "apps/mfind",
+        "apps/mmeter",
+        "apps/mmod",
+        "apps/mrep",
+        "apps/msholl",
+        "apps/mshow",
+    ],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+    ],
+    keywords=(
+        "neuron",
+        "morphology",
+        "reconstruction",
+        "morphometry"),
+)

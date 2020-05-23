0ad768d4ed5b3557dcee3b46828334f437c58bf7
Alexander Kozlov <akozlov@kth.se>
2020-05-23 18:45:42 +0200

    20200523

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setuptools.setup(name="morphon",
-    version="0.0.7dev1",
+    version="0.0.7",
     description="Neuron morphology processing scripts",
     long_description=readme(),
     author="Alexander Kozlov",
